40f26758f5973949e380b8c057749aae6c868917
Monty Anderson <monty.linux@gmail.com>
2017-07-17 13:34:55 +0100

    uses web3 provider objects

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const stdrpc = require("stdrpc");
 const abi = require("ethereumjs-abi");
+const rpc = require("./lib/rpc");
 
 module.exports = class Ethane {
-	constructor() {
-		this.rpc = stdrpc(...arguments);
+	constructor(provider) {
+		this.rpc = rpc(provider);
 	}
 
 	contract(contractAbi) {
@@ -11,15 +11,15 @@ module.exports = class Ethane {
 			this.address = address;
 		};
 
-		Contract.prototype.rpc = this.rpc;
+		Contract.prototype.ethane = this;
 
-		contractAbi.forEach(method => {
+		for(let method of contractAbi) {
 			const inputTypes = method.inputs.map(i => i.type);
 			const outputTypes = (method.outputs || []).map(i => i.type);
 			const id = abi.methodID(method.name, inputTypes).toString("hex");
 
 			Contract.prototype[method.name] = function() {
-				return this.rpc.eth_coinbase().then(coinbase => {
+				return this.ethane.rpc.eth_coinbase().then(coinbase => {
 					const args = {
 						from: coinbase,
 						to: this.address,
@@ -27,9 +27,9 @@ module.exports = class Ethane {
 					};
 
 					if(method.constant == true) {
-						return this.rpc.eth_call(args, "latest");
+						return this.ethane.rpc.eth_call(args, "latest");
 					} else {
-						return this.rpc.eth_sendTransaction(args);
+						return this.ethane.rpc.eth_sendTransaction(args);
 					}
 				}).then(res => {
 					const output = abi.rawDecode(outputTypes, Buffer.from(res.slice(2), "hex"));
@@ -37,7 +37,7 @@ module.exports = class Ethane {
 					return output.length <= 1 ? output[0] : output;
 				});
 			};
-		});
+		}
 
 		return Contract;
 	}
